3848fa8990414cd84d69289c07124f8e9cdf5a4f
joshwlivingston <joshwlivingston@gmail.com>
2025-03-24 07:50:46 -0600

    finish ex-02-05-traingle

--- ch02-looping-and-counting/ex-02-05-traingle.cpp
+++ ch02-looping-and-counting/ex-02-05-traingle.cpp
@@ -15,46 +15,24 @@ int main()
     const int MAX_TRIES = 5;
     int counter = 0;
 
+    string::size_type dim_length = 0;
+    // throws std::length_error on non-integer || <= 1
     cout << "Enter the traingle's side's length: ";
-    string::size_type dim_length;
-    cin >> dim_length;    
-    
-    while (dim_length % 2 == 0 || dim_length == 1)
-    {
-        if (counter == MAX_TRIES) {
-            cout << endl << "Max attempts reached. Exiting program." << endl;
-            return 1;
-        }
+    cin >> dim_length;
 
-        cout << "Please enter an odd number > 1 for the traingle's side's length: ";
-        cin >> dim_length;
-        ++counter;
-    }
+    string::size_type dim_last_index = dim_length - 1;
+    string::size_type whitespace_side_length = dim_last_index;
 
-    const string::size_type padding_side_extra = (dim_length - 2) / 2 + 1;
+    // top row
+    const string whitespace(whitespace_side_length, ' ');
+    cout << whitespace << "*" << endl;
+    whitespace_side_length -= 1;
 
+    // middle rows
     string::size_type whitespace_middle_length = 1;
-    string::size_type whitespace_side_length = dim_length / 2 + padding_side_extra;
-    for (int i = 0; i != dim_length; ++i)
+    
+    for (int i = 1; i != dim_last_index; ++i)
     {
-        if (i == 0) {
-            // top row
-            const string whitespace(whitespace_side_length, ' ');
-            cout << whitespace << "*" << endl;
-            whitespace_side_length -= 1;
-            continue;
-        }
-
-        if (i == dim_length - 1) {
-            // bottom row
-            for (int j = 0; j != dim_length - 1; ++j)
-            {
-                cout << "* ";
-            }
-            cout << "*" << endl;
-            break;
-        }
-
         const string whitespace_side(whitespace_side_length, ' ');
         const string whitespace_middle(whitespace_middle_length, ' ');
 
@@ -63,6 +41,13 @@ int main()
         whitespace_side_length -= 1;
         whitespace_middle_length += 2;
     }
+
+    // bottom row
+    for (int i = 0; i != dim_last_index; ++i)
+    {
+        cout << "* ";
+    }
+    cout << "*" << endl;
     
     return 0;
 }
