40bb4ad96a40b7d34ddd12f62e9ea5d893934caa
Josh Livingston <joshwlivingston@gmail.com>
2025-04-06 09:52:53 -0600

    Finish Chapter 7 Section 3

---
+++ ch07-associative-containers/sec-07-03/README.md
@@ -0,0 +1,12 @@
+# Accelerated C++
+## Chapter 7 Section 3
+
+Our aim is to create a program that generates an index of where words occur in a given input.
+To achieve this, we create a map of each word that show the number of occurrences by line.
+This functionality is built in `build_word_index()`, in `word_index.cc`.
+
+Our implementation differs slightly from the book, which records the lines as a vector. Using
+the map, however, allows for the same information tob be stored, but more efficiently.
+
+A **default argument** is used in `build_word_index()` (see `word_index.h`), which allows the 
+programmer to omit the argument if desired, as I did in `main.cc`.
--- ch07-associative-containers/sec-07-03/word_index.cc
+++ ch07-associative-containers/sec-07-03/word_index.cc
@@ -11,22 +11,28 @@ using std::map;
 using std::string;
 using std::vector;
 
+/*
+Given an input stream of strings, returns an index showing the occurrences of each word by line.abort
+*/
 word_index build_word_index(
     istream &in,
     vector<string> get_words(const string &))
 {
+    // initialize output index
+    word_index out;
+
+    // read each line in input stream
     string line;
     int line_number = 0;
-    map<string, map<int, int>> out;
-
     while (getline(in, line))
     {
         ++line_number;
 
-        vector<string> words = get_words(line);
-
+        // iterate through all words identified in line;
+        const vector<string> words = get_words(line);
         for (vector<string>::const_iterator word = words.begin(); word != words.end(); ++word)
         {
+            // increment entry for that word on that line
             ++out[*word][line_number];
         }
     }
