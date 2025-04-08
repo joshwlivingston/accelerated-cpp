6f58fe579b418b9381cd6bec1751bd7aa3b67f36
Josh Livingston <joshwlivingston@gmail.com>
2025-04-07 18:16:07 -0600

    a happier path

--- ch07-associative-containers/ex-07-06-mad-libs-no-recursion/mad_libs.cc
+++ ch07-associative-containers/ex-07-06-mad-libs-no-recursion/mad_libs.cc
@@ -43,50 +43,58 @@ MadLibs generate_mad_libs(const MadLibsRules &mad_libs_rules)
         string::iterator char_start = find_if(rule_stack[i].begin(), rule_stack[i].end(), is_not_space);
 
         // invariant: char_start marks the initial position of the portion of the rule that has yet to be parsed
-        while (char_start != rule_stack[i].end())
+        while (char_start != rule_stack[i].end() && !is_bracket_opener(*char_start))
         {
-            if (is_bracket_opener(*char_start)) // 1.
-            {
-                const string::iterator char_end = find_if(char_start, rule_stack[i].end(), is_space);
-                if (is_bracket_closer(*(char_end - 1))) // 2.
-                {
-                    const MadLibsRules::const_iterator rule_list = mad_libs_rules.find(string(char_start, char_end));
-                    if (rule_list != mad_libs_rules.end()) // 3.
-                    {
-                        /*
-                        All true:
-                        1. first character is <
-                        2. last character in this word is >
-                        3. This <word> is a MadLibs rule list entry
-                        */
-
-                        // obtain random rule from rule list
-                        rule new_rule = rule_list->second[nrand(rule_list->second.size())];
-
-                        if (char_end != rule_stack[i].end())
-                        {
-                            // portions of this rule have not been parsed yet
-
-                            // preserve unparsed portion of rule in stack
-                            new_rule = new_rule + string(char_end, rule_stack[i].end());
-                        }
-
-                        // append new rule to stack and begin processing it
-                        rule_stack.push_back(new_rule);
-                        break;
-                    }
-                }
-            }
-
-            // scan rule until potential new rule defintion is found
-            const string::iterator char_end = find_if(char_start, rule_stack[i].end(), is_bracket_opener);
-
-            // add everything until next rule to MadLibs output
-            mad_libs.push_back(string(char_start, char_end));
+            // we are not inside a MadLibs rule tag, so we have to find the end of this string and add it to the output
+            const string::iterator output_char_end = find_if(char_start, rule_stack[i].end(), is_bracket_opener);
+            mad_libs.push_back(string(char_start, output_char_end));
 
             // continue scanning the rule
-            char_start = char_end;
+            char_start = output_char_end;
         }
+
+        if (char_start == rule_stack[i].end())
+        {
+            // we have reached end of string
+            ++i;
+            continue;
+        }
+
+        /*
+        If we are not at the end of string, then, per the while() condition, this character is a <.
+
+        So, we should look for a MadLibs rule tag.
+        */
+
+        const string::iterator char_end = find_if(char_start, rule_stack[i].end(), is_space);
+        if (!is_bracket_closer(*(char_end - 1)))
+        {
+            // 2. last character in this word is not >
+            ++i;
+            continue;
+        }
+
+        /*
+        Now we know we have something that looks like a <tag>, but is it an entry in our MadLibs rules?
+        */
+
+        const MadLibsRules::const_iterator rule_list = mad_libs_rules.find(string(char_start, char_end));
+        if (rule_list == mad_libs_rules.end())
+        {
+            // this <tag> is not a MadLibs rule entry
+            ++i;
+            continue;
+        }
+
+        /*
+        If reach here, we have stumbled upon a MadLibs rule entry, and need to process it.
+
+        So, we need to obtain a random rule from the list and add it to the rule stack.
+        */
+
+        const rule portion_of_rule_still_unparsed = char_end != rule_stack[i].end() ? string(char_end, rule_stack[i].end()) : "";
+        const rule newly_obtained_rule = rule_list->second[nrand(rule_list->second.size())];
+        rule_stack.push_back(newly_obtained_rule + portion_of_rule_still_unparsed);
         ++i;
     }
 
