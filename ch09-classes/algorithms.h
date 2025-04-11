80ec3f293f87b31c06e773f279dada1d044f513e
Josh Livingston <joshwlivingston@gmail.com>
2025-04-11 08:41:41 -0600

    some more styling

--- ch09-classes/algorithms.h
+++ ch09-classes/algorithms.h
@@ -7,21 +7,20 @@ namespace algo {
 
 /* Checks whether [begin, end) and a range starting from comparison are
    equal */
-template <class InputIteratorInput, class InputIteratorComparison>
-bool equal(InputIteratorInput begin, InputIteratorInput end,
-           InputIteratorComparison comparison) {
+template <class InputIteratorIn, class InputIteratorCmp>
+bool equal(InputIteratorIn begin, InputIteratorIn end, InputIteratorCmp comp) {
     while (begin != end)
-        if (*begin++ != *comparison++)
+        if (*begin++ != *comp++)
             return false;
     return true;
 }
 
 /* Searches for the first occurrence of the sequence of elements
    [matches_begin, matches_end) in the range [begin, end) */
-template <class InputIteratorInput, class InputIteratorMatches>
-InputIteratorInput search(InputIteratorInput begin, InputIteratorInput end,
-                          InputIteratorMatches matches_begin,
-                          InputIteratorMatches matches_end) {
+template <class InputIteratorIn, class InputIteratorMatches>
+InputIteratorIn search(InputIteratorIn begin, InputIteratorIn end,
+                       InputIteratorMatches matches_begin,
+                       InputIteratorMatches matches_end) {
     for (; begin != end; ++begin) {
         InputIteratorMatches match_current = matches_begin;
         while (match_current != matches_end)
@@ -31,21 +30,21 @@ InputIteratorInput search(InputIteratorInput begin, InputIteratorInput end,
     return begin;
 }
 
-/* Returns an iterator to the first element in the range [begin, end) that
-   is equal to match (or last if there is no such iterator) */
-template <class InputIteratorInput, class TypeInput>
-InputIteratorInput find(InputIteratorInput begin, InputIteratorInput end,
-                        const TypeInput &match) {
+/* Returns an iterator to the first element in the range [begin, end) that is
+   equal to match (or last if there is no such iterator) */
+template <class InputIteratorIn, class TypeInput>
+InputIteratorIn find(InputIteratorIn begin, InputIteratorIn end,
+                     const TypeInput &match) {
     while (begin != end && *begin != match)
         ++begin;
     return begin;
 }
 
-/* Returns an iterator to the first element in the range [begin, end) for
-   which predicate() is true (or last if there is no such iterator) */
-template <class InputIteratorInput, class TypeInput>
-InputIteratorInput find_if(InputIteratorInput begin, InputIteratorInput end,
-                           bool predicate(const TypeInput &)) {
+/* Returns an iterator to the first element in the range [begin, end) for which
+   predicate() is true (or last if there is no such iterator) */
+template <class InputIteratorIn, class TypeInput>
+InputIteratorIn find_if(InputIteratorIn begin, InputIteratorIn end,
+                        bool predicate(const TypeInput &)) {
     while (begin != end && !predicate(*begin))
         ++begin;
     return begin;
@@ -53,10 +52,9 @@ InputIteratorInput find_if(InputIteratorInput begin, InputIteratorInput end,
 
 /* Copies the elements in the range, defined by [begin, end), to another range
    beginning at destination_begin (copy destination range). */
-template <class InputIteratorInput, class OutputIteratorDestination>
-OutputIteratorDestination copy(InputIteratorInput begin,
-                               InputIteratorInput end,
-                               OutputIteratorDestination destination_begin) {
+template <class InputIteratorIn, class OutputIteratorOut>
+OutputIteratorOut copy(InputIteratorIn begin, InputIteratorIn end,
+                       OutputIteratorOut destination_begin) {
     while (begin != end)
         *destination_begin++ = *begin++;
     return destination_begin;
@@ -65,14 +63,10 @@ OutputIteratorDestination copy(InputIteratorInput begin,
 /* Copies the elements in the range, defined by [begin, end), to another range
    beginning at destination_begin (copy destination range), omitting elements
    equal to match. */
-template <class InputIteratorInput,
-          class OutputIteratorDestination,
-          class TypeInput>
-OutputIteratorDestination remove_copy(
-    InputIteratorInput begin,
-    InputIteratorInput end,
-    OutputIteratorDestination destination_begin,
-    const TypeInput &match) {
+template <class InputIteratorIn, class OutputIteratorOut, class TypeInput>
+OutputIteratorOut remove_copy(InputIteratorIn begin, InputIteratorIn end,
+                              OutputIteratorOut destination_begin,
+                              const TypeInput &match) {
     for (; begin != end; ++begin)
         if (*begin != match)
             *destination_begin++ = *begin;
@@ -82,14 +76,10 @@ OutputIteratorDestination remove_copy(
 /* Copies the elements in the range, defined by [begin, end), to another range
    beginning at destination_begin (copy destination range), omitting elements
    for which predicate() is true. */
-template <class InputIteratorInput,
-          class OutputIteratorDestination,
-          class TypeInput>
-OutputIteratorDestination remove_copy_if(
-    InputIteratorInput begin,
-    InputIteratorInput end,
-    OutputIteratorDestination destination_begin,
-    bool predicate(const TypeInput &)) {
+template <class InputIteratorIn, class OutputIteratorOut, class TypeInput>
+OutputIteratorOut remove_copy_if(InputIteratorIn begin, InputIteratorIn end,
+                                 OutputIteratorOut destination_begin,
+                                 bool predicate(const TypeInput &)) {
     for (; begin != end; ++begin)
         if (!predicate(*begin))
             *destination_begin++ = *begin;
@@ -98,28 +88,23 @@ OutputIteratorDestination remove_copy_if(
 
 /* Removes all elements that equal match from the range [begin, end) and
    returns a past-the-end iterator for the new end of the range. */
-template <class ForwardIteratorInput, class TypeInput>
-ForwardIteratorInput remove(ForwardIteratorInput begin,
-                            ForwardIteratorInput end,
-                            const TypeInput &match) {
-    ForwardIteratorInput first_removed = algo::find(begin, end, match);
+template <class ForwardIteratorIn, class TypeInput>
+ForwardIteratorIn remove(ForwardIteratorIn begin, ForwardIteratorIn end,
+                         const TypeInput &match) {
+    ForwardIteratorIn first_removed = algo::find(begin, end, match);
     for (begin = first_removed; begin != end; ++begin)
         if (*begin != match)
             *first_removed++ = std::move(*begin);
     return first_removed;
 }
 
-/* Applies the given function to the the range [begin, end), and
-   stores the result in an output range starting from begin_destination */
-template <class InputIteratorInput,
-          class OutputIteratorDestination,
-          class TypeInput,
+/* Applies the given function to the the range [begin, end), and stores the
+   result in an output range starting from begin_destination */
+template <class InputIteratorIn, class OutputIteratorOut, class TypeInput, 
           class TypeOutput>
-OutputIteratorDestination transform(
-    InputIteratorInput begin,
-    InputIteratorInput end,
-    OutputIteratorDestination begin_destination,
-    TypeOutput transform_function(const TypeInput &)) {
+OutputIteratorOut transform(InputIteratorIn begin, InputIteratorIn end,
+                            OutputIteratorOut begin_destination,
+                            TypeOutput transform_function(const TypeInput &)) {
     while (begin != end)
         *begin_destination++ = transform_function(*begin++);
     return begin_destination;
@@ -129,11 +114,10 @@ OutputIteratorDestination transform(
    elements for which the predicate p returns true precede all elements for
    which predicate() returns false. Relative order of the elements is not
    preserved. */
-template <class ForwardIteratorInput, class TypeInput>
-ForwardIteratorInput partition(ForwardIteratorInput begin,
-                               ForwardIteratorInput end,
-                               bool predicate(const TypeInput &)) {
-    ForwardIteratorInput true_partition_end =
+template <class ForwardIteratorIn, class TypeInput>
+ForwardIteratorIn partition(ForwardIteratorIn begin, ForwardIteratorIn end,
+                            bool predicate(const TypeInput &)) {
+    ForwardIteratorIn true_partition_end =
         algo::find_if(begin, end, predicate);
     for (begin = true_partition_end; begin != end; ++begin)
         if (predicate(*begin))
@@ -150,6 +134,7 @@ T accumulate(InputIterator begin, InputIterator end, T init) {
     return init;
 }
 
+/* Computes the mean of the elements in the range [begin, end). */
 template <class InputIterator, class T>
 T mean(InputIterator begin, InputIterator end) {
     return accumulate<InputIterator, T>(begin, end, 0) / (end - begin);
